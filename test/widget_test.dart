92ebf12993d891a4c1936b9830d586f4a60e3164
MatheusFranco <matheus.franco@evolutionsoft.com.br>
2024-05-06 10:53:53 -0300

    feat: atualizado pacotes para gerar apk e adicionado permissão acesso INTERNET

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:zendays/Telasiniciais/main.dart';
+import 'package:zendays/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
